c7e54784769f52e13e873fc7ed9d7deead06a59d
Brian Scramlin <scramlo@hotmail.com>
2015-01-10 15:22:15 -0600

    fixed jquery turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
 //= require masonry/jquery.masonry
-//= require jquery.turbolinks
 //= require_tree .
