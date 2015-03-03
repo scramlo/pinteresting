2f1de5ca394c329422022f43daec308c378eae9a
Brian Scramlin <scramlo@hotmail.com>
2015-03-03 07:07:53 -0600

    Added validations and changed emails to first/last name

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,5 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, :styles => { :full => "100%", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  validates :image, :description, presence: true
 end
